9233a26d1b5919a6e087ec4423a9679e608bae2b
pammelaAC <pammelaavila@gmail.com>
2023-10-10 19:58:40 -0700

    components created with styles

---
+++ src/app/components/small-cards/small-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-cards',
+  templateUrl: './small-cards.component.html',
+  styleUrls: ['./small-cards.component.css']
+})
+export class SmallCardsComponent {
+
+}
