fd7c004d9af55251e7783d68550de641514bc8be
pammelaAC <142856786+pammelaAC@users.noreply.github.com>
2023-10-12 15:26:02 +0000

    components dinamics propertys

--- src/app/components/small-cards/small-cards.component.ts
+++ src/app/components/small-cards/small-cards.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-cards',
   templateUrl: './small-cards.component.html',
   styleUrls: ['./small-cards.component.css']
 })
-export class SmallCardsComponent {
+export class SmallCardsComponent implements OnInit {
 
+  @Input()
+  photoCover:string="https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  
+  @Input()
+  cardTitle:string = "NOVA SÉRIE ANUNCIADA"
+  constructor () {}
+
+  ngOnInit(): void {
+    
+  }
 }
